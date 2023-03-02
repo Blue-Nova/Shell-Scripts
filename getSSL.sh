88aacef6c828d01c47eac2625153d5c7a37de469
Blue-Nova <105223497+Blue-Nova@users.noreply.github.com>
2023-03-02 20:43:11 +0100

    Update getSSL.sh

--- getSSL.sh
+++ getSSL.sh
@@ -7,6 +7,6 @@ sudo apt install certbot python3-certbot-apache -y
 sudo ufw allow 80
 sudo ufw allow 443
 
-read -p "Enter your domain: " $DOMAIN
+read -p 'Enter your domain: ' DOMAIN
 
 sudo certbot --apache -d $DOMAIN -d www.$DOMAIN
